9cf3561445a875bd041d60c432278c9d85edd420
Eric Tossell <erictossell@gmail.com>
2023-03-30 17:48:59 -0400

    Added basic logging functionality

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@
 import asyncio
 import os
 import discord
+import logging
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -22,6 +23,13 @@ intents.presences = True #v2
 bot = commands.Bot(command_prefix='!', intents=intents, activity=discord.Activity(type=discord.ActivityType.watching, name="for !help"))
 
 cogs_list = ['commands.general', 'commands.poll', 'events.errors', 'commands.raiderIO', 'events.memberEvents']
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.WARNING)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 
 def load_extensions():
     for cog in cogs_list:
