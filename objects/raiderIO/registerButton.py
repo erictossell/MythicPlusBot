6b523819d5739f5279c86b526b36236b5f2159b5
Eric Tossell <erictossell@gmail.com>
2023-03-31 15:26:40 -0400

    Unregister modal completed

--- objects/raiderIO/registerButton.py
+++ objects/raiderIO/registerButton.py
@@ -1,8 +1,20 @@
 import discord
 
 from objects.raiderIO.registerModal import RegisterModal
+from objects.raiderIO.unregisterModal import UnregisterModal
 
 class RegisterButton(discord.ui.View):
+    
+    
+    @discord.ui.button(label='Unregister', style=discord.ButtonStyle.red, emoji='🗑️')
+    async def unregister_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+        try: 
+            await interaction.response.send_modal(UnregisterModal(title="Unregister your character"))
+            
+        except Exception as e:
+            print('Error occured:', e)
+            await interaction.message.delete()
+            await interaction.response.send_message("Error occured while processing your request. Please try again later.", ephemeral=True)
     
     
     @discord.ui.button(label='Register', style=discord.ButtonStyle.green, emoji='📄')
--- objects/raiderIO/unregisterModal.py
+++ objects/raiderIO/unregisterModal.py
@@ -1,19 +1,18 @@
-import discord 
 import csv
+import discord
+
 from objects.raiderIO.raiderIOService import RaiderIOService
 filename = './members.csv'
-
-class RegisterModal(discord.ui.Modal):
+class UnregisterModal(discord.ui.Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
-        self.add_item(discord.ui.InputText(label="What is your character name?", required= True))
-        self.add_item(discord.ui.InputText(label="What is your realm? (Default is Area-52)", required= False))
+    
+        self.add_item(discord.ui.InputText(label="Which character do you want to unregister?", required= True))
+        self.add_item(discord.ui.InputText(label="Character Realm: (Defaults to Area-52)", required= False))
         
     async def callback(self, interaction: discord.Interaction):
-        
         try:
-            name = self.children[0].value.capitalize()  
+            name = self.children[0].value.capitalize()
             realm = self.children[1].value.capitalize()
             userID = interaction.user.id
             userName = interaction.user.name
@@ -26,19 +25,22 @@ class RegisterModal(discord.ui.Modal):
                 await interaction.response.send_message('Character '+ name +' on ' + realm + ' not found', ephemeral=True)
                 return
             
-            else:            
-                with open(filename, mode='r')as csv_file:
+            else:
+                with open(filename, mode='r') as csv_file:
                     reader = csv.reader(csv_file, delimiter=',')
-                    for row in reader:
-                        if row[0] == name and row[1] == realm:
-                            await interaction.response.send_message(name + ' on realm ' + realm + ' has already been registered', ephemeral=True)
-                            break
-                    else:
-                        with open(filename, mode='a', newline='') as csv_file:
-                            writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                            writer.writerow([name, realm, userID, userName])            
-                        await interaction.response.send_message('You have registered the character ' + name + ' on realm ' + realm + ' for Tal-Bot reporting.', ephemeral=True)
+                    data = list(reader)
                     
+                for row in data:
+                    if row[0] == name and row[1] == realm and row[2] == str(userID):
+                        data.remove(row)
+                        with open(filename, mode='w', newline='') as csv_file:
+                            writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                            writer.writerows(data)
+                        await interaction.response.send_message('You have unregistered the character ' + name + ' on realm ' + realm + ' for Tal-Bot reporting.', ephemeral=True)
+                        break
+                    
+                    
+                        
         except Exception as e:
             user = await self.ctx.bot.fetch_user(173958345022111744)
             channel = await user.create_dm()
