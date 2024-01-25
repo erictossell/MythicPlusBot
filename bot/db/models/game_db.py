3932b45658be917f8162251c8b794e6d85679329
Eric Tossell <erictossell@gmail.com>
2024-01-25 09:44:36 -0500

    imports

--- bot/db/models/game_db.py
+++ bot/db/models/game_db.py
@@ -2,25 +2,21 @@ import datetime
 from sqlalchemy import DateTime, Column, Integer, String
 from sqlalchemy.orm import relationship
 from bot.db.base import Base
+from bot.db.game_guild_db import GameGuildDB
 
 
 class GameDB(Base):
-    
-    __tablename__ = 'games'
-    
+    __tablename__ = "games"
+
     id = Column(Integer, primary_key=True)
-    
+
     name = Column(String)
-    modified_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)    
-    
+    modified_at = Column(
+        DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow
+    )
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
     game_guilds = relationship("GameGuildDB", back_populates="game")
-    
-    
-    def __init__(self,
-                 name: str = 'World of Warcraft'
-    ):
+
+    def __init__(self, name: str = "World of Warcraft"):
         self.name = name
-        
-    
-    
